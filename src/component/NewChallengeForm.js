c48e8a3da56ef7eb1aa7d518b4dd260b1420634e
aschemily <aschemily@gmail.com>
2019-06-18 15:38:59 -0400

    submit function

--- src/component/NewChallengeForm.js
+++ src/component/NewChallengeForm.js
@@ -9,37 +9,52 @@ class NewChallengeForm extends Component {
     location:''
   }
 
-  inputChange = (event)=>{
-  //  const {title, image, information, location} = event.target
-  //console.log('event.target.value',event.target.value)
+  inputChange = (event) =>{
+    const {title, image, information, location} = event.target
+  //  console.log('event.target.value',event.target.value)
+    this.setState({
+      title: event.target.value,
+      image: event.target.value,
+      information: event.target.value,
+      location: event.target.value
+    })
   }
 
+handleSubmit = () =>{
+  this.props.newChallenge(this.state.title, this.state.image, this.state.information, this.state.location)
+}
+
 render(){
+  console.log('what are props', this.props)
   return(
 
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
          <Form.Field>
            <label>Image</label>
              <input type="text" placeholder='Image'
-               onChange={this.inputChange} />
+               onChange={this.inputChange}
+               value = {this.state.image}/>
            </Form.Field>
 
            <Form.Field>
              <label>Title</label>
              <input placeholder='Title'
-               onChange={this.inputChange}/>
+               onChange={this.inputChange}
+               value={this.state.title}/>
            </Form.Field>
 
            <Form.Field>
              <label>Location</label>
              <input placeholder='Location'
-               onChange={this.inputChange}/>
+               onChange={this.inputChange}
+               value={this.state.location}/>
            </Form.Field>
 
          <Form.Field>
            <label>Information</label>
            <input placeholder='Information'
-             onChange={this.inputChange} />
+             onChange={this.inputChange}
+             value={this.state.information} />
          </Form.Field>
 
          <Button type='submit'>Submit</Button>
--- src/container/challengeContainer.js
+++ src/container/challengeContainer.js
@@ -20,18 +20,44 @@ class challengeContainer extends Component{
     })
   }
 
+  newChallenge = (title, image, information, location)=>{
+    console.log('what is?', title, image, information, location)
+    fetch("http://localhost:3000/api/v1/challenges",{
+      method: 'POST',
+      headers:{
+         'Content-Type': 'application/json',
+         'Accept': 'application/json'
+       },
+       body: JSON.stringify({
+         title: title,
+         image: image,
+         information: information,
+         location: location,
+         user_id: 1
+       })
+    })
+    .then(r => r.json())
+    .then(data =>{
+      console.log('data', data)
+    })
+  }
+
+
+
   render(){
   //  console.log('challengeContainer props', this.props)
     return(
       <div>
       <h1> Challenges Container </h1>
       <Modal trigger={<Button>New Challenge</Button>}>
-        <Modal.Header><NewChallengeForm/></Modal.Header>
+        <Modal.Header><NewChallengeForm newChallenge={this.newChallenge}/></Modal.Header>
       </Modal>
 
       {this.state.challenges.map(challenge=>{
-      return <Challenge key={uuid()} challenge={challenge}/>
+      return <Challenge key={uuid()} challenge={challenge} />
       })}
+
+
 
       </div>
     )
